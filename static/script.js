319c62c0e376730eb1a7d42605f470549b86c581
AmrendraOG <amren.dev@gmail.com>
2025-07-26 19:53:57 +0530

    Correct FETCH address

--- static/script.js
+++ static/script.js
@@ -8,7 +8,7 @@ function analyzeText() {
         return
     }
 
-    fetch("http://127.0.0.1:5000/analyze", {
+    fetch("https://sentiment-analysis-dyxp.onrender.com/analyze", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
